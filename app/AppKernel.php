e45efe18d76caa1725e6641fe2466bf146b80ff9
Decebal D <decebal1988@gmail.com>
2015-03-15 08:58:26 +0200

    remove moopa bootstrap bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,7 +19,6 @@ class AppKernel extends Kernel
 
             //api bundles
             new ApiBundle\ApiBundle(),
-            new Mopa\Bundle\BootstrapBundle\MopaBootstrapBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
